6b25629947a9c053fb639cef6f60317f2e37ca53
Parikshit2mote <parikshitmote333@gmail.com>
2026-06-03 23:09:01 +0530

    App.jsx fetch gallery

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [error, setError] = useState(null);
   const [modalApod, setModalApod] = useState(null);
 
-  // Fetch today's APOd
+  // Fetch today's APO
   useEffect(() => {
     if (view === "today") {
       setLoading(true);
